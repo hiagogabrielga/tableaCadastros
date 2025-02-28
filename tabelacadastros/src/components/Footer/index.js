72eaebf9abde935c5f0efd5e5122d2135d4b36b2
hiagogabrielga <hiagogaberiel1132@gmail.com>
2025-02-28 16:27:20 -0400

    atualizar

--- tabelacadastros/src/components/Footer/index.js
+++ tabelacadastros/src/components/Footer/index.js
@@ -2,16 +2,16 @@ import styles from './footer.module.css'
 
 export default function Footer() {
     return (
-        <div>
-            <div>
+        <div className={styles.containerFooter}>
+            <div className={styles.containerMyContats}>
                 <h2>Redes sociais</h2>
-                <ul>
-                    <li><a href="https://github.com/hiagogabrielga" target='_blank'>GitHub</a></li>
-                    <li><a href="https://www.instagram.com/hiago.gabriel.940098/" target='_blank'>Instagem</a></li>
-                    <li><a href="https://www.linkedin.com/in/hiago-gabriel-94a687336/" target='_blank'>Linkedin</a></li>
+                <ul className={styles.listMyConstats}>
+                    <li className={styles.optionContat}><a className={styles.ancoraContat} href="https://github.com/hiagogabrielga" target='_blank'>GitHub</a></li>
+                    <li className={styles.optionContat}><a className={styles.ancoraContat} href="https://www.instagram.com/hiago.gabriel.940098/" target='_blank'>Instagem</a></li>
+                    <li className={styles.optionContat}><a className={styles.ancoraContat} href="https://www.linkedin.com/in/hiago-gabriel-94a687336/" target='_blank'>Linkedin</a></li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.copyright}>
                 <h3>hiagogabrielga 2025 &copy;</h3>
             </div>
         </div>
